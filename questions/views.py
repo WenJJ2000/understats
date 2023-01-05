1cb9c8769f6857fc12443e4ddce193b3209d1dbf
Junwei <junwei@meshbio.com>
2023-01-05 18:07:57 +0800

    added documentations

--- questions/views.py
+++ questions/views.py
@@ -2,31 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import DecisionTreeNode
 
-# from .forms import QuestionForm
-
-# class TreeNode:
-#     def __init__(self,value,left=None, right=None):
-#         self.value = value
-#         self.left = left
-#         self.right = right
-    
-# def my_view(request):
-#     root = TreeNode
-
-# def question_view(request):
-#     form = QuestionForm(request.POST or None)
-#     # if form.is_valid():
-#         # form.save()
-#     context = {
-#         "form": form
-#     }
-#     return render(request,"questions/form.html", context)
-
-# def decision_tree_view(request):
-#     root_node = DecisionTreeNode.objects.get(pk=2)  # assume this is the root node of the decision tree
-#     context = {"root_node": root_node}
-#     return render(request, "questions/form.html", context)
-
 def decision_tree_view(request):
     # Fetch the root node of the decision tree
     root_node = get_object_or_404(DecisionTreeNode, pk=1)
@@ -56,7 +31,7 @@ def decision_tree_view(request):
         nodes.append(current_node)
         nodes.append(next_node)
         
-        # Retrieve the previous nodes and add it to the front of nodes. After the full recursion, nodes will have the entire pathway of nodes to the current node
+        # Retrieve the previous nodes and add it to the front of nodes. After the full recursion, the list "nodes" will have the entire pathway of nodes to the current node
         def retrieve_previous_nodes(node):
             for x in all_nodes:
                 if x.yes_node_id == node.id:
@@ -83,15 +58,4 @@ def decision_tree_view(request):
     # Render the form template with the list of nodes and the current node
     return render(request, 'questions/form.html', {'nodes': nodes})
 
-# Create your views here.
-# def decision_tree_view(request):
-#     current_node = DecisionTreeNode.objects.get(id=1)  # get the root node
 
-#     if request.method == 'POST':
-#         answer = request.POST.get('answer')
-#         if answer == 'yes':
-#             current_node = current_node.yes_node_id
-#         else:
-#             current_node = current_node.no_node_id
-
-#     return render(request, 'questions/form.html', {'current_node': current_node})
